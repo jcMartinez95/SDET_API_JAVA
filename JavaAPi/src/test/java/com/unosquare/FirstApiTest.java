be57a43fdf3eeac14d0994aac8ece3eb31354ccd
jcMartinez95 <carlos.martinez@unosquare.com>
2022-06-21 18:54:15 -0500

    Second Pull Request Assignment

--- JavaAPi/src/test/java/com/unosquare/FirstApiTest.java
+++ JavaAPi/src/test/java/com/unosquare/FirstApiTest.java
@@ -5,16 +5,76 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import java.io.FileReader;
 
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.BeforeMethod;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class FirstApiTest {
-	@Test
+	
+	@Test(priority = 2, enabled = true)
+	  public void firstPostTest() {
+		  
+		//------------------ Second Pull Request ------------------
+		Reporter.log("--------- Second Pull Request --------- <p>");  
+		
+		int aux = 0;
+		String path = null;
+		String endPoint = null;
+		
+		do {
+		//JSON file path
+			switch(aux) {
+			case 0: path = CurrentPath("\\src\\main\\java\\Json\\Register.json");
+					endPoint = "/register";
+					break;
+			case 1: path = CurrentPath("\\src\\main\\java\\Json\\Login.json");
+					endPoint = "/login";
+					break;		
+			}
+			
+		JSONParser parser = new JSONParser();
+		
+		try {
+			Object obj = parser.parse(new FileReader(path));
+	
+			// Convert read JSON file to an object
+			JSONObject jsonObject = (JSONObject) obj;
+	
+			//Add object headers & body read from JSON file
+			RequestSpecification httpRequest = RestAssured.given(); 
+			httpRequest.headers("Content-Type", "application/json"); 
+			httpRequest.body(jsonObject.toString()); 
+			//Response obtained with POST request
+			Response response = httpRequest.post(endPoint);
+			System.out.println("Response: " + response.asPrettyString());
+			
+			//Add to report
+			// Body
+			Reporter.log("Response Body: " + response.body().asPrettyString() + "<p>");
+			// Status code
+			Reporter.log("Status Code: " + response.statusCode() + "<p>");
+			// JSON Body being sent
+			Reporter.log("JSON Body being posted: " + jsonObject + "<p>");
+			// URL 
+			Reporter.log("URL: " + RestAssured.baseURI + "/users" + "<p>");
+			// End
+			Reporter.log("------------------------------------- <p>");
+			  
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		aux ++;
+		}while(aux<2);
+	}
+	
+	@Test(priority = 1, enabled = false)
 	  public void firstPullTest() {
 		  
 		//------------------ First Pull Request ------------------
@@ -22,7 +82,7 @@ public class FirstApiTest {
 		Reporter.log("--------- First Approach --------- <p>");
 		
 		//First Approach  
-		RestAssured.baseURI = "https://reqres.in/api/";
+		
 		RequestSpecification httpRequest = RestAssured.given();
 		Response response = httpRequest.get("/users/2");
 		
@@ -71,13 +131,22 @@ public class FirstApiTest {
 		  		.log().body()
 		  		.extract().response();
 		
-		Reporter.log("Response Content: " + secondResponse.getBody().asString());
+		Reporter.log("Status Code: " + secondResponse.getStatusCode() + "<p>");
+		Reporter.log("Content Type: " + secondResponse.getContentType() + "<p>");
+		Reporter.log("Response Content: " + secondResponse.getBody().asString() + "<p>");
 		
 		}
 
+private static String CurrentPath(String path)
+  {
+	  System.getProperty("user.dir");
+      String fileName = System.getProperty("user.dir") + path;
+      return fileName;
+  }
   
   @BeforeMethod
   public void beforeMethod() {
+	  RestAssured.baseURI = "https://reqres.in/api";
   }
 
 }
